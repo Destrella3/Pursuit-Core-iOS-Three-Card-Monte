dcc645290d95e124813f52581cfbe16c9e4cd393
Destrella3 <destrella385@gmail.com>
2018-11-06 16:31:50 -0500

    update

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstCard: UIButton!
+    @IBOutlet weak var secondCard: UIButton!
+    @IBOutlet weak var thirdCard: UIButton!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            firstCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
+        case 1:
+            secondCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
+        case 2:
+           thirdCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
+        default:
+        print("invalid tag")
+        }
+    }
+    
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    firstCard.isEnabled = true
+    secondCard.isEnabled = true
+    thirdCard.isEnabled = true
+    
   }
 
 
